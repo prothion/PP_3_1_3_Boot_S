6020d53c0b15e864296f1e66454f4d7a22fb96ce
Vadim <109858454+prothion@users.noreply.github.com>
2022-12-19 12:06:21 +0300

    Update User.java

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -49,7 +49,7 @@ public class User implements UserDetails {
     @Column(name="timeRegistration")
     private LocalDateTime timeRegistration;
 
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
